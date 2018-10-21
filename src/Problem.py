d8ae0873055df2e1ab3f3e83774b37aa47050740
Konrad_Maciej_Szymański <konrad.w.szymanski@student.put.poznan.pl>
2018-10-21 11:27:55 +0200

    cleanup

--- src/Problem.py
+++ src/Problem.py
@@ -5,9 +5,11 @@ class Problem:
     def __init__(self, lines: str, boundary: float):
         self.tasks = []
         for line in lines:
-            filtered_line_data = filter(lambda element: len(element) > 0, line.strip().split())
-            data = list(map(lambda element: int(element), filtered_line_data))
-            self.tasks.append(Task(data[0], data[1], data[2]))
+            self.__load_task_from_line(line)
         self.problem_processing_time = sum(map(lambda element: element.processing_time, self.tasks))
         self.common_due_date = int(self.problem_processing_time * boundary)
 
+    def __load_task_from_line(self, line: str):
+        filtered_line_data = filter(lambda element: len(element) > 0, line.strip().split())
+        data = list(map(lambda element: int(element), filtered_line_data))
+        self.tasks.append(Task(data[0], data[1], data[2]))
--- src/main.py
+++ src/main.py
@@ -3,15 +3,20 @@ import os
 from ProblemsReader import ProblemsReader
 
 BOUNDARY = 0.2
+FILE_PATH = "../resources/data/sch10.txt"
+
+
+def get_current_directory():
+    return os.path.dirname(os.path.abspath(__file__))
 
 
 def main():
-    package_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(package_dir, "../resources/sch10.txt")
+    package_dir = get_current_directory()
+    file_path = os.path.join(package_dir, FILE_PATH)
     problems = ProblemsReader.read(file_path, BOUNDARY)
     for problem in problems:
-        print("Processing time: " + str(problem.problem_processing_time) +
-              ", Common due date: " + str(problem.common_due_date))
+        print("Processing time: {}, Common due date: {}"
+              .format(problem.problem_processing_time, problem.common_due_date))
 
 
 if __name__ == "__main__":
